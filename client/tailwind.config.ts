212fb5e9703dbabdf5756cdbf33d494c887880e2
Wishing-Qi <wishingqi@outlook.com>
2025-10-01 21:25:00 +0800

    Update tailwind.config.ts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -5,15 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: ['selector','[data-color-mode="dark"]'],
-  colors: {
-    'theme': '#8dd1d2',           // 原色 +10% 明度
-    'theme-hover': '#76c0c1',     // +5% 明度
-    'theme-active': '#5ba9aa',    // -10% 明度
-    'background': {
-      light: '#f4fdfd',           // 极浅薄荷
-      dark: '#102828',            // 深墨绿
-    },
-    'dark': '#1f3132',
+  theme: {
+    extend: {
+      colors: {
+        'theme': '#8dd1d2',           // 原色 +10% 明度
+        'theme-hover': '#76c0c1',     // +5% 明度
+        'theme-active': '#5ba9aa',    // -10% 明度
+        'background': {
+          light: '#f4fdfd',           // 极浅薄荷
+          dark: '#102828',            // 深墨绿
+        },
+        'dark': '#1f3132',
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/typography'),
